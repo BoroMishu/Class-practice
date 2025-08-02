3a204984abede327263674cf36fde1c45f48f822
22-49921-3@student.aiub.edu <22-49921-3@student.aiub.edu>
2025-08-03 02:59:39 +0600

    lab code

---
+++ Downloads/Practice/Java1.js
@@ -0,0 +1,12 @@
+let str1="@";
+if(str1.includes('@'))
+{
+    console.log("It's a valid email")
+}
+else
+{
+    console.log("It's not a valid email")
+}
+
+let str2="maria binte ";
+console.log(str2.trimEnd());
